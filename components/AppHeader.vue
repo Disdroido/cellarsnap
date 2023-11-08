32f68ce11b5d8d34acfad881a167e17fffff8248
EthanWorthAhoy <ethanworth@ahoyclub.com>
2023-11-09 09:33:00 +1100

    final mobile nav

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -5,26 +5,32 @@
 </script>
 
 <template>
-  <div class="navbar bg-bunker-950 h-20 z-10">
+  <div class="navbar bg-bunker-950 h-20 z-10 sticky top-0 left-0 right-0">
     <Notifications />
 
-    <div class="flex-1">
-      <NuxtLink to="/" class="pl-5 normal-case text-xl">CellarSnap</NuxtLink>
-    </div>
-    <div class="flex-1 flex-row justify-end justify-items-end">
-      <ul class="menu menu-horizontal px-3 md:flex gap-3 justify-end hidden">
-        <li v-if="user"><NuxtLink to="/dashboard">Dashboard</NuxtLink></li>
-        <li><NuxtLink to="/pricing">Pricing</NuxtLink></li>
-        <li v-if="!user"><NuxtLink to="/signin">Sign In</NuxtLink></li>
-        <li v-if="!user"><NuxtLink to="/signup">Start for free</NuxtLink></li>
-      </ul>
-      <UserAccount v-if="user" :user="user" />
-      <UButton label="Open" @click="navigationOpen = true" class="md:hidden" />
+    <div class="flex flex-1 flex-row content-center px-5">
+      <div class="flex-1">
+        <NuxtLink to="/" class="normal-case text-xl">CellarSnap</NuxtLink>
+      </div>
+      <div class="flex flex-1 flex-row justify-end justify-items-end items-center gap-4 md:gap-0">
+        <ul class="menu menu-horizontal px-3 md:flex gap-3 justify-end hidden">
+          <li v-if="user"><NuxtLink to="/dashboard">Dashboard</NuxtLink></li>
+          <li><NuxtLink to="/pricing">Pricing</NuxtLink></li>
+          <li v-if="!user"><NuxtLink to="/signin">Sign In</NuxtLink></li>
+          <li v-if="!user"><NuxtLink to="/signup">Start for free</NuxtLink></li>
+        </ul>
+        <UserAccount v-if="user" :user="user" />
+        <button @click="navigationOpen = true" class="md:hidden flex flex-col gap-[6px] h-7 w-7 text-[#ebfaf2] justify-center">
+          <span class="w-7 h-0.5 text-[#ebfaf2] bg-current transform duration-150"></span>
+          <span class="w-5 h-0.5 text-[#ebfaf2] bg-current hover:w-7 hover:text-tawny-port-900 transform duration-150"></span>
+          <span class="w-7 h-0.5 text-[#ebfaf2] bg-current transform duration-150"></span>
+        </button>
+      </div>
     </div>
   </div>
 
   <USlideover v-model="navigationOpen">
-    <UCard class="flex flex-col flex-1" :ui="{ background: 'bg-shark-950 dark:bg-shark-950', body: { base: 'flex-1' }, header: { padding: 'pt-7 px-4 sm:px-6' }, ring: '', divide: '' }">
+    <UCard class="flex flex-col flex-1" :ui="{ background: 'bg-shark-950 dark:bg-shark-950', rounded: '', body: { base: 'flex-1' }, header: { padding: 'pt-7 px-4 sm:px-6' }, ring: '', divide: '' }">
       <template #header>
         <div class="flex justify-end">
           <button @click="navigationOpen = false" class="">
@@ -46,22 +52,50 @@
       <div class="h-full flex flex-col gap-4">
         <NuxtLink
           to="/dashboard"
-          class="w-full shadow-lg text-center rounded-md py-3 text-white border border-[#B82133] hover:bg-[#B82133] duration-200"
+          class="w-full shadow-lg text-center rounded-md py-3 text-white border-2 border-tawny-port-900 hover:bg-tawny-port-900 duration-200"
           :class="{
-            'bg-[#B82133]': $route.path === '/dashboard'
+            'bg-tawny-port-900': $route.path === '/dashboard'
           }"
           @click="navigationOpen = false"
-          >Dashboard</NuxtLink
-        >
+          v-if="user"
+          >Dashboard
+        </NuxtLink>
         <NuxtLink
           to="/mycellars"
-          class="w-full shadow-lg text-center rounded-md py-3 text-white border border-[#B82133] hover:bg-[#B82133] duration-200"
+          class="w-full shadow-lg text-center rounded-md py-3 text-white border-2 border-tawny-port-900 hover:bg-tawny-port-900 duration-200"
           :class="{
-            'bg-[#B82133]': $route.path === '/mycellars'
+            'bg-tawny-port-900': $route.path === '/mycellars'
           }"
           @click="navigationOpen = false"
-          >My Cellars</NuxtLink
-        >
+          v-if="user"
+          >My Cellars
+          </NuxtLink>
+        <NuxtLink to="/pricing" 
+          class="w-full shadow-lg text-center rounded-md py-3 text-white border-2 border-tawny-port-900 hover:bg-tawny-port-900 duration-200" 
+          :class="{
+            'bg-tawny-port-900': $route.path === '/mycellars'
+          }"
+          @click="navigationOpen = false"
+          >Pricing
+        </NuxtLink>
+        <div class="flex flex-col gap-4 justify-end h-full">
+          <NuxtLink to="/signin" 
+            class="w-full shadow-lg text-center rounded-md py-3 text-white border-2 border-tawny-port-900 hover:bg-tawny-port-900 duration-200" 
+            :class="{
+              'bg-tawny-port-900': $route.path === '/mycellars'
+            }"
+            v-if="!user" @click="navigationOpen = false"
+            >Sign In
+          </NuxtLink>
+          <NuxtLink to="/signup" 
+            class="w-full shadow-lg text-center rounded-md py-3 text-white bg-tawny-port-900 hover:bg-tawny-port-900 duration-200" 
+            :class="{
+              'bg-[#B82133]': $route.path === '/mycellars'
+            }"
+            v-if="!user" @click="navigationOpen = false"
+            >Start for free
+          </NuxtLink>
+        </div>
       </div>
     </UCard>
   </USlideover>
